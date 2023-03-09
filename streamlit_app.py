f0c0ac257e3ea983ab40348cee031948e37f5d9a
Ben <101144776+benlode@users.noreply.github.com>
2023-03-09 11:11:31 -0600

    Update streamlit_app.py
    normalized json response and put output into a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,15 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalizes the json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# changes the output to a table
+streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
+
+
+
